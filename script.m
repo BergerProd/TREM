9b3f415394d6070dbeb4a9c19d7dd37514f2558d
BergerProd <quentin.berge13@gmail.com>
2021-01-26 21:37:35 +0100

    en cours sur cas 3, illumination sur cas 2, au lieu de faire recherche indice, il suffit de faire interp1(y,z,0)

--- script.m
+++ script.m
@@ -1,3 +1,7 @@
+%Transport & Melange
+%Resolution Numérique
+%Quentin BERGE & Vincent BERGNES
+%ENSEEIHT - 3A MFEE - SEE
 %-------------------------------
 % Donnees Entree
 %-------------------------------
@@ -7,9 +11,6 @@ cas=2;
 %cas 1 = Jet sans stratification
 %cas 2 = Panache force avec stratification
 %cas 3 = Hauteur terminale
-
-
-
 
 % Physique
 z0=0; %debut du domaine : m
@@ -223,6 +224,57 @@ ylabel('g')
 %title('Solutions transitoire de u et v dans le plan u,v')
 grid on
 hold off
+
+elseif cas==3
+%% Influence des conditions initiales sur le cas du panache forcé avec stratification
+disp("Hauteur Terminal : Panache force avec stratification")
+
+
+%y(1)=Q
+%y(2)=M
+%y(3)=F
+
+% Conditions initiales
+y0 = [[pi pi 10*pi],[pi pi 0],[pi 0 0]];
+N=[0.05, 1, 0]; % Frequence Brunt Vaissala
+
+for i=1:3
+    
+%valeurs par defaut avant passage dans la boucle while
+indicateur_1=0;
+indicateur_2=0;
+
+    while(indicateur_1 ==0  || indicateur_2==0) %si indicateur 1=0 ou indicateur 2=0
+        L=L+10;%augmentation du domaine        
+                
+        %Zspan entre min et max
+        zspan=[z0 L];
+        %Appel ODE45
+        [z,y] = ode45(@(z,y)odefun(z,y,lambda,N(i)),zspan,y0(i));
+        
+        %Creation des grandeurs b,w,g
+        b=(((y(:,1).^2))/(pi*y(:,2)))^(1/2);
+        w=y(:,2)./(y(:,1));
+        g=(y(:,3)./y(:,1));
+        
+        
+        
+        % Recherche des annulations
+        
+        % Flux de flottabilité
+        indice_annulation=recherche_indice_annulation(y(:,3));
+        if indice_annulation ~= 0
+            fprintf("le flux de flottabilité s annule entre %6.2f m et %6.2f m \n", z(indice_annulation), z(indice_annulation+1))
+            indicateur_1=1;
+        else
+            disp("pas d'annulation de la flottabilité dans l'intervalle, augmentation du domaine de 10 m")
+            indicateur_1=0;
+        end
+    end
+    ht(i)=z(indice_annulation)
+end
+    
+
 else
     disp("erreur saisie cas cas =1,2 ou 3")
 end
@@ -260,6 +312,7 @@ dydz(2)=(y(3)/y(1))*(y(1)^2/(pi*y(2)));
 dydz(3)=-N^2*y(1)/(pi);
 end
 
+% renvoit l'indice ou cela passe par 0
 function indice_annulation=recherche_indice_annulation(vecteur)
 for i=1:length(vecteur)-1
     prod=vecteur(i)*vecteur(i+1);
